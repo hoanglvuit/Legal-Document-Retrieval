f4d738e0f1cb9f1c727ac48d2080ac6b78bb4f10
hoanglv <hoanglv282004@gmail.com>
2025-05-20 23:08:12 +0700

    Bi Encoder

--- src/train_biencoder.py
+++ src/train_biencoder.py
@@ -1,63 +1,84 @@
 import pandas as pd
 import torch
-from torch.utils.data import DataLoader
 from sentence_transformers import SentenceTransformer,SentenceTransformerTrainer,SentenceTransformerTrainingArguments
 from sentence_transformers.training_args import BatchSamplers
 from sentence_transformers import SentenceTransformer, SentenceTransformerTrainer, losses
 from datasets import Dataset,load_dataset
+import argparse
+import os
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
-# Load dataset
-train_df = pd.read_csv('../data/processed/train.csv',encoding='utf-8') 
-train_ques = train_df['question'].tolist() 
-train_ans = train_df['answer'].tolist() 
-eval_df = pd.read_csv('../data/processed/train.csv',encoding='utf-8') 
-eval_ques = eval_df['question'].tolist() 
-eval_ans = eval_df['answer'].tolist() 
+def load_data(input_dir): 
+    for dirpath, dirname, filenames in os.walk(input_dir): 
+        if "train.csv" in filenames: 
+            train_df = pd.read_csv(os.path.join(dirpath, "train.csv"), encoding="utf-8") 
+        if "eval.csv" in filenames: 
+            eval_df = pd.read_csv(os.path.join(dirpath, "eval.csv"), encoding="utf-8") 
+    train_question, train_answer = train_df["question"].tolist(), train_df["answer"].tolist()
+    eval_question, eval_answer = eval_df["question"].tolist(), eval_df['answer'].tolist() 
+    return train_question, train_answer, eval_question, eval_answer
 
-# Define data
-train_data = {'query': train_ques,
-                'answer': train_ans} 
-eval_data = {'query': eval_ques, 
-                'answer': eval_ans} 
+
+if __name__ == "__main__": 
+    parser = argparse.ArgumentParser(description="Train Bi Encoder") 
+    parser.add_argument("--input_dir", type=str, default="../data/processed", help="Folder contain train.csv and eval.csv") 
+    parser.add_argument("--model_name", type=str, default= "bkai-foundation-models/vietnamese-bi-encoder") 
+    parser.add_argument("--num_epochs", type=int, default=3) 
+    parser.add_argument("batch_size", type=int, default=32) 
+    parser.add_argument("lr", type=float, default=2e-5) 
+    parser.add_argument('weight_decay', type=float, default=0.01) 
+    parser.add_argument("output_dir", type=str, default="../saved_model/BiEncoder")
+
+
+args = parser.parse_args()
+# Load data
+train_question, train_answer, eval_question, eval_answer = load_data(args.input_dir)
+
+
+# Define dataset
+train_data = {'query': train_question,
+                'answer': train_answer} 
+eval_data = {'query': eval_question, 
+                'answer': eval_answer} 
 train_dataset = Dataset.from_dict(train_data).shuffle(seed=28)
 eval_dataset = Dataset.from_dict(eval_data)
 
-# Bi-encoder
-model = SentenceTransformer('bkai-foundation-models/vietnamese-bi-encoder')
-
+# Define model and loss
+model = SentenceTransformer(args.model_name)
 loss = losses.MultipleNegativesRankingLoss(model)
 
-
-args = SentenceTransformerTrainingArguments(
-    output_dir="models/bi_encoder", 
-    num_train_epochs=3,
-    per_device_train_batch_size=32,
-    per_device_eval_batch_size=32,
-    learning_rate=2e-5,
-    warmup_ratio=0, 
+# Define hyperparameters
+train_args = SentenceTransformerTrainingArguments(
+    output_dir= args.output_dir, 
+    num_train_epochs= args.num_epochs,
+    per_device_train_batch_size= args.batch_size,
+    per_device_eval_batch_size= args.batch_size,
+    learning_rate= args.lr, 
     fp16=True,  
     bf16=False,  
     batch_sampler=BatchSamplers.NO_DUPLICATES,  
-    eval_strategy="steps",
-    eval_steps=750,
-    save_strategy="steps",
-    save_steps = 750,
+    eval_strategy="epoch",
+    eval_steps=1,
+    save_strategy="epoch",
+    save_steps = 1,
     save_total_limit = 3,
     load_best_model_at_end=True,                
     metric_for_best_model="eval_loss",       
-    greater_is_better=False,  
-    logging_steps=200,
-    weight_decay =0.01,
+    greater_is_better=False, 
+    logging_strategy ='epochs',
+    logging_steps=1,
+    weight_decay = args.weight_decay,
     report_to= "none",
+    log_level = 'error'
 )
+
 trainer = SentenceTransformerTrainer(
     model=model,
-    args=args,
+    args=train_args,
     train_dataset=train_dataset,
     eval_dataset=eval_dataset,
-    loss=loss,
-)
+    loss=loss)
+
 trainer.train()
-model.save_pretrained('models/bi_encoder/best')
+model.save_pretrained(os.path.join(args.output_dir, 'best'))
