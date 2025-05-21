3a2f3dd7a79f4152a11d3090d754d3c81354d9e3
hoanglv <hoanglv282004@gmail.com>
2025-05-21 17:54:50 +0700

    push result

--- evaluation.py
+++ evaluation.py
@@ -1,12 +1,12 @@
 import os 
 import argparse 
 import pandas as pd
-from .utils import exist_m, mrr_m
+from src.utils import exist_m, mrr_m
 
 if __name__ == "__main__": 
     parser = argparse.ArgumentParser(description="Evaluate result.txt on exist and mrr metrics")
-    parser.add_argument('pred_path', type=str, help="Path to file .txt") 
-    parser.add_argument('true_path', type=str, help="Path to file .csv")
+    parser.add_argument('--pred_path', type=str, help="Path to file .txt") 
+    parser.add_argument('--true_path', type=str, help="Path to file .csv")
     parser.add_argument('--top_e', type=int, default=90, help='The number of top candidates to evaluate by exist_m')
     parser.add_argument('--top_m', type=int, default=10, help='The number of top candidates to evaluate by mrr_m')
 
