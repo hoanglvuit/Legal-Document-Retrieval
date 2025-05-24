5a4334db247620c6d3bdc96014d5b65a94fb4a48
hoanglv <hoanglv282004@gmail.com>
2025-05-24 22:04:54 +0700

    update

--- run.py
+++ run.py
@@ -11,14 +11,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Run with a legal question")
     parser.add_argument('--question', type=str) 
     parser.add_argument('--database', type=str, default= 'data/database') 
+    parser.add_argument('--bi_path', type=str, default= 'saved_model/BiEncoder/model1/best')
+    parser.add_argument('--cross_path', type=str, default= 'saved_model/CrossEncoder/model1')
+    parser.add_argument('corpus_path', type=str, default= 'data/processed/corpus.csv')
     args = parser.parse_args()
 
     # load BiEncoder 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    bi_model = SentenceTransformer('saved_model/BiEncoder/model1/best')
+    bi_model = SentenceTransformer(args.bi_path)
 
     # load data 
-    corpus = pd.read_csv('data/processed/corpus.csv', encoding='utf-8') 
+    corpus = pd.read_csv(args.corpus_path, encoding='utf-8') 
     documents = corpus['text'].tolist()
 
     # database 
@@ -35,7 +38,7 @@ if __name__ == '__main__':
     retrieval_docs = [documents[i] for i in top_inds]
     
     # load Cross Encoder 
-    cross_model = CrossEncoder('saved_model/CrossEncoder/model1', max_length=256)
+    cross_model = CrossEncoder(args.cross_path, max_length=256)
     cross_model.model.half()
     
     pairs = [[args.question, doc] for doc in retrieval_docs] 
